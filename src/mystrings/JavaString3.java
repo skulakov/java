7eb382bfc954ccfd419e85c2249e642d566703e7
user <user@student303-20>
2019-03-05 19:54:35 +0200

    test

--- src/mystrings/JavaString3.java
+++ src/mystrings/JavaString3.java
@@ -14,6 +14,28 @@ import mytestclass.*;
 public class JavaString3 {
 
     public static void main(String[] args) {
+        String word = new String("java");
+        
+        String text1 = "The origins of the name \"Java\" are not clear. One possibility is that the island was named after ";
 
+        
+        
+        String[] words2 = text1.split("\\W+");
+        
+        
+        System.out.format("Слов в тексте  =  %d%n", words2.length);
+
+        
+        for(String w: words2){
+            System.out.printf(w + "; ");
+        }
+        System.out.println("\n-------------");
+        
+         System.out.printf("Используем CompareTo(%s,%s)", word, words2[0], word.equalsIgnoreCase(words2[0]));
+         
+          System.out.println("\n-------------");
+        
+         System.out.printf("Используем CompareTo(%s,%s)", word, words2[0], word.compareTo(words2[0]));
+        
     }
 }
